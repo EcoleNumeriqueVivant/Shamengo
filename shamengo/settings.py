99c8bbd060943091209c70f74f62d2078417d5a0
bcoston <benedicte.coston@gmail.com>
2018-04-25 12:42:08 +0200

    add django-scheduler/ css KO

--- shamengo/settings.py
+++ shamengo/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'schedule',
+    'djangobower',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +109,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+#STATICFILES_FINDERS = [
+#	'djangobower.finders.BowerFinder',
+#]
+
+BOWER_INSTALLED_APPS = (
+    'jquery',
+    'jquery-ui',
+    'bootstrap'
+)
+
+STATIC_URL = '/static/'
+
+BOWER_COMPONENTS_ROOT = '/Users/ben/Desktop/SHAMENGO/Shamengo-arnaud/Shamengo/components/'
+
+
