d10e1efd725590285dcf1d69d4cbb1bfbbbeb87d
etienne ziegelmeyer <etiennezpro@gmail.com>
2018-04-30 22:53:20 +0200

    Remove old migration & initialize a new one

--- shamengo/settings/dev.py
+++ shamengo/settings/dev.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'scheduler',
 ]
 
@@ -121,7 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'scheduler/static'),
 ]
 
