964e31fc865bfdd5877b7a22ed44fb5082ffbba4
etienne ziegelmeyer <etiennezpro@gmail.com>
2018-04-30 21:59:28 +0200

    dd created line on models

--- scheduler/models.py
+++ scheduler/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Entry(models.Model):
     name = models.CharField(max_length=100)
     date = models.DateTimeField()
-    description = models.DateTimeField(auto_now_add=True)
-    
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
