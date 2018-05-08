2ef94f64c1850ae1f8717979eaa58cb8b7edf38a
etienne ziegelmeyer <etiennezpro@gmail.com>
2018-05-08 10:47:43 +0200

    Add category to models Entry & *.pyc to gitignore

--- scheduler/models.py
+++ scheduler/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 
 class Entry(models.Model):
+
+    CATEGORY = (
+        ("A", "atelier"),
+        ("Co", "cocktail"),
+        ("C", "conference"),
+        ("E", "evenement"),
+        ("M", "meetup"),
+        ("P", "projection"),
+        ("R", "reunion"),
+    )
     name = models.CharField(max_length=100)
     date = models.DateTimeField()
     description = models.TextField()
-    category = models.TextField()
+    category = models.CharField(max_length=2 , choices=CATEGORY)
     created = models.DateTimeField(auto_now_add=True)
