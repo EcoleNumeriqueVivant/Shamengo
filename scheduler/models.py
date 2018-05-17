3959e8e6b1f08200d5df866bad95b153598fa385
etienne ziegelmeyer <etiennezpro@gmail.com>
2018-05-17 22:02:41 +0200

    Add color Category by entry

--- scheduler/models.py
+++ scheduler/models.py
@@ -8,7 +8,7 @@ class Entry(models.Model):
     name = models.CharField(max_length=100)
     date = models.DateTimeField()
     description = models.TextField()
-    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -16,8 +16,8 @@ class Entry(models.Model):
 
 
 class Category(models.Model):
-    # class Meta:
-    #     verbose_name_plural = 'categories'
+    class Meta:
+        verbose_name_plural = 'categories'
 
     name = models.CharField(max_length=35)
     color = ColorField(default='#FF0000')
