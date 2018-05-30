11eb1c6a2e32a6781bbe837ae28c0f0b60860765
Marie Hubert <marie@vaio-linux.home>
2018-05-30 23:47:07 +0200

    prendre en compte le status draft, publie ou offline dans l'affichage des evenements en ligne

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from scheduler.models import Entry, Category
-import datetime 
+import datetime
 
 
 def calender(request):
-	entries = Entry.objects.all().order_by('date_start').exclude(date_end__lt=datetime.datetime.now())
+	entries = Entry.objects.all().order_by('date_start').exclude(status='d').exclude(status='o').exclude(date_end__lt=datetime.datetime.now())
 	if not entries :
 		return render(request, 'no_event.html', {'entries': entries})
 	else :
